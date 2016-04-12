7e2c7cb3a1a09ca684dbfce9e556b1b0927def47
Tobias Moser <tobi@sose.ch>
2016-04-12 14:29:20 +0200

    food as well

--- food.py
+++ food.py
@@ -11,12 +11,14 @@ class Food:
         self.npp = npp
         self.max_food = max_food
 
+        self.food_size = food_size
+
     def do_action(self):
         self.grow()
 
     def grow(self):
-        offset = int(food_size / 2)
-        x_size, y_size = grid.shape
+        offset = int(self.food_size / 2)
+        x_size, y_size = self.grid.shape
         y_min = (self.y - offset) % y_size
         y_max = (self.y + offset) % y_size
 
@@ -26,6 +28,6 @@ class Food:
         self.grid[x_min:x_max,y_min:y_max] += self.npp
 
         exceed = self.grid[x_min:x_max,y_min:y_max] > self.max_food
-        self.grid[exceed] = max_food
+        self.grid[exceed] = self.max_food
 
 
